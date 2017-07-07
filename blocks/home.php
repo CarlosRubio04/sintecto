cf13d23199d5788e9c8719054f59e8ff834ad752
Carlos Rubio Mac BM <carlosblondsk8@gmail.com>
2017-07-07 18:25:57 -0500

    hero

---
+++ blocks/home.php
@@ -0,0 +1,70 @@
+<section class="hero">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<img src="img/logo_security_vision.png" data-src="img/logo_security_vision.png | img/logo_security_vision@2x.png" alt="" class="lazyImg center-block">
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-8 col-lg-8 col-sm-12 col-xs-12">
+				<article>
+					<h1>Nosotros</h1>
+					<p>Compañia líder con más de 17 de años de <br>
+						experiencia liderando procesos en asesoría, <br>
+						consultoría e investigación de los principales <br>
+						riesgos de seguridad, la información que <br>
+						necesita para elegir correctamente.</p>
+					</article>
+				</div>
+				<div class="col-md-4 col-lg-4 col-sm-12 col-xs-12">
+					<?php require_once('blocks/formulario.php'); ?>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<section class="propouse">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12 tittle">
+					<h2>Nuestra propuesta integral</h2>
+					<p>
+						Integramos nuestro equipo de profesionales y la más robusta plataforma tecnológica para facilitar a su compañia tomar la mejor desición. <br>
+						Logre a través de nuestra metodología:
+					</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+					<div class="tumbns">
+						<figure>
+							<img src="img/002_chart.png" data-src="002_chart.png | 002_chart@2x.png" alt="" class="lazyImg center-block">
+							<figcaption>
+								Implementar un esquema <span>personalizado</span> a la realidad e intereses de su negocio.
+							</figcaption>
+						</figure>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+					<div class="tumbns">
+						<figure>
+							<img src="img/002_chart.png" data-src="002_chart.png | 002_chart@2x.png" alt="" class="lazyImg center-block">
+							<figcaption>
+								Implementar un esquema <span>personalizado</span> a la realidad e intereses de su negocio.
+							</figcaption>
+						</figure>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+					<div class="tumbns">
+						<figure>
+							<img src="img/002_chart.png" data-src="002_chart.png | 002_chart@2x.png" alt="" class="lazyImg center-block">
+							<figcaption>
+								Implementar un esquema <span>personalizado</span> a la realidad e intereses de su negocio.
+							</figcaption>
+						</figure>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
